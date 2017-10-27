ce8be7de7de39fdf9bef60d514194efcab6ef19f
asfda <abc@gmail.com>
2017-10-27 19:52:23 +0900

    20171027 カメラ回転作成はじめ　和田

--- Assets/Scripts/Main/Doll_Challenge/Noroishika/Break_Cursed.cs
+++ Assets/Scripts/Main/Doll_Challenge/Noroishika/Break_Cursed.cs
@@ -5,15 +5,69 @@ using UnityEngine;
 /*
 呪い人形は5回タップすると破壊される
 */
-public class Break_Cursed : MonoBehaviour {
+public class Break_Cursed : MonoBehaviour
+{
+    int count = 0;  // タップ数のカウント
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        TouchInfo info = AppUtil.GetTouch();
+
+        // タップを取得
+        if (info == TouchInfo.Began)
+        {
+            // Debug.Log(Input.mousePosition + ":"+ count);
+
+            GameObject target = null;   // タップしたオブジェクトの取得
+
+            // タップ位置とオブジェクトをタップしたかの取得
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            // タップしたオブジェクトを取得する
+            if (Physics.Raycast(ray, out hit))
+            {
+                target = hit.collider.gameObject;
+            }
+
+            if(hit.collider.gameObject.name == "test_doll" && count == 4)
+            {
+                Debug.Log(hit.collider.gameObject);
+                count += 1;
+            }
+        }
+        if(info == TouchInfo.Ended)
+        {
+            GameObject target = null;   // タップしたオブジェクトの取得
+
+            // タップ位置とオブジェクトをタップしたかの取得
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            // タップしたオブジェクトを取得する
+            if (Physics.Raycast(ray, out hit))
+            {
+                target = hit.collider.gameObject;
+            }
+
+            if (hit.collider.gameObject.name == "test_doll")
+            {
+                Debug.Log(hit.collider.gameObject);
+                count += 1;
+            }
+        }
+
+        // タップが5回で破壊
+        if(count == 5)
+        {
+            MeshRenderer mesh = GetComponent<MeshRenderer>();
+
+            mesh.material.color = new Color(0, 0, 0, 0.0f);
+        }
 	}
 }
--- Assets/Scripts/Main/Doll_Challenge/Swipe_Dolls.cs
+++ Assets/Scripts/Main/Doll_Challenge/Swipe_Dolls.cs
@@ -6,15 +6,60 @@ using UnityEngine;
 人形を３回タッチ・スワイプする。受け取った指令で内容は変わる
 ３回タップし終わると、 3-呪い人形の数 だけの人形を入手する
 */
-public class Swipe_Dolls : MonoBehaviour {
+public class Swipe_Dolls : MonoBehaviour
+{
+    private float touchPos;
+    private float endPos;
+
+    float rotate_Z;
+
+    public int speed;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        //Rigidbody rigid = GetComponent<Rigidbody>();
+
+        TouchInfo info = AppUtil.GetTouch();
+
+        if (info == TouchInfo.Began)
+        {
+            Debug.Log(Input.mousePosition);
+
+            touchPos = AppUtil.GetTouchPosition().x;
+        }
+        else if(info == TouchInfo.Ended)
+        {
+            endPos = AppUtil.GetTouchPosition().x;
+
+            if(endPos - touchPos > 0)
+            {
+                Debug.Log(endPos - touchPos);
+                /*
+                Rigidbody rb = GetComponent<Rigidbody>();
+                Vector3 vec = ;
+                /*
+                float next_x = vec.x * Mathf.Cos(Mathf.Deg2Rad * -1) - vec.z * (Mathf.Sin(Mathf.Deg2Rad * -1));
+                float next_z = vec.x * Mathf.Sin(Mathf.Deg2Rad * -1) + vec.z * (Mathf.Cos(Mathf.Deg2Rad * -1));
+                rb.velocity = new Vector3(next_x, rb.velocity.y, next_z);
+                
+                Quaternion target = Quaternion.LookRotation(vec);
+
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    target,
+                    Time.deltaTime * speed
+                );*/
+                /*
+                float angle = Mathf.LerpAngle(transform.rotation.x, endPos / 2, Time.time * speed);
+                transform.eulerAngles = new Vector3(0, angle, 0);*/
+            }
+        }
+    }
 }
